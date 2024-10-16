83cfe6287d385942b241d6147217f38e6094d7db
mah0x211 <mah0x211@gmail.com>
2024-10-16 09:06:13 +0900

    Code formatting with luaformatter

--- lib/item.lua
+++ lib/item.lua
@@ -1,5 +1,5 @@
 --[[
-  
+
   Copyright (C) 2014 Masatoshi Teruya
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -8,10 +8,10 @@
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
- 
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
- 
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -19,122 +19,107 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
-   
+
   lib/item.lua
   lua-session
-  
+
   Created by Masatoshi Teruya on 14/12/08.
-  
---]]
 
--- module
-local typeof = require('util.typeof');
-local Cookie = require('cookie');
-local Proxy = require('session.proxy');
+--]] -- module
+local typeof = require('util.typeof')
+local Cookie = require('cookie')
+local Proxy = require('session.proxy')
 -- class
-local Item = require('halo').class.Item;
+local Item = require('halo').class.Item
 
+function Item:init(cfg, sid)
+    local own = protected(self)
+    local data, err
 
-function Item:init( cfg, sid )
-    local own = protected( self );
-    local data, err;
-    
     if sid == nil then
-        data = {};
+        data = {}
     else
-        data, err = cfg.store:get( sid );
+        data, err = cfg.store:get(sid)
         if err then
-            return nil, err;
+            return nil, err
         elseif data == nil then
-            return nil;
-        -- data must be table
-        elseif not typeof.table( data ) then
-            return nil, 'acquired data is corrupted: data type is not table.';
+            return nil
+            -- data must be table
+        elseif not typeof.table(data) then
+            return nil, 'acquired data is corrupted: data type is not table.'
         end
     end
-    
-    own.store = cfg.store;
-    own.idgen = cfg.idgen;
-    own.cookie = cfg.cookie;
-    own.ttl = cfg.ttl;
-    own.sid = sid;
-    own.data = data;
-    
-    return self;
+
+    own.store = cfg.store
+    own.idgen = cfg.idgen
+    own.cookie = cfg.cookie
+    own.ttl = cfg.ttl
+    own.sid = sid
+    own.data = data
+
+    return self
 end
 
+function Item:proxy() return Proxy.new(protected(self).data) end
 
-function Item:proxy()
-    return Proxy.new( protected( self ).data );
-end
+function Item:save(genCookie, regenId)
+    local own = protected(self)
+    local oldsid, sid, ok, err
 
-
-function Item:save( genCookie, regenId )
-    local own = protected( self );
-    local oldsid, sid, ok, err;
-    
     -- regenerate session id: default true
     if regenId ~= false then
-        oldsid = own.sid;
-        own.sid = own.idgen();
-    -- generate session id if nil
+        oldsid = own.sid
+        own.sid = own.idgen()
+        -- generate session id if nil
     elseif own.sid == nil then
-        own.sid = own.idgen();
+        own.sid = own.idgen()
     end
-    
+
     -- generate cookie: default true
     if genCookie ~= false then
-        sid, err = Cookie.bake( own.cookie.name, own.sid, own.cookie );
-        if err then
-            return nil, err;
-        end
+        sid, err = Cookie.bake(own.cookie.name, own.sid, own.cookie)
+        if err then return nil, err end
     else
-        sid = own.sid;
+        sid = own.sid
     end
-    
+
     -- save data
-    ok, err = own.store:set( own.sid, own.data, own.ttl );
+    ok, err = own.store:set(own.sid, own.data, own.ttl)
     if err then
-        own.sid = oldsid or own.sid;
-        return nil, err;
-    -- delete old session
+        own.sid = oldsid or own.sid
+        return nil, err
+        -- delete old session
     elseif oldsid then
-        ok, err = own.store:delete( oldsid );
+        ok, err = own.store:delete(oldsid)
         if err then
-            own.sid = oldsid;
-            return nil, err;
+            own.sid = oldsid
+            return nil, err
         end
     end
-    
-    return sid;
+
+    return sid
 end
 
+function Item:destroy(genCookie)
+    local own = protected(self)
+    local sid, ok, err
 
-function Item:destroy( genCookie )
-    local own = protected( self );
-    local sid, ok, err;
-    
     -- generate null data cookie: default true
     if genCookie ~= false then
-        sid = Cookie.bake( own.cookie.name, '', own.cookie );
-        if err then
-            return nil, err;
-        end
+        sid = Cookie.bake(own.cookie.name, '', own.cookie)
+        if err then return nil, err end
     else
-        sid = own.sid;
+        sid = own.sid
     end
-    
+
     -- delete data
     if own.sid then
-        ok, err = own.store:delete( own.sid );
-        if err then
-            return nil, err;
-        end
+        ok, err = own.store:delete(own.sid)
+        if err then return nil, err end
     end
-    
-    return sid;
+
+    return sid
 end
 
-
-return Item.exports;
+return Item.exports
 
--- session.lua
+++ session.lua
@@ -1,5 +1,5 @@
 --[[
-  
+
   Copyright (C) 2014 Masatoshi Teruya
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -8,10 +8,10 @@
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
- 
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
- 
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -19,121 +19,105 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
-   
+
   session.lua
   lua-session
-  
-  Created by Masatoshi Teruya on 14/12/08.
-  
---]]
 
--- module
-local Cookie = require('cookie');
-local siphash48 = require('siphash').encode48;
-local gettimeofday = require('process').gettimeofday;
-local typeof = require('util.typeof');
-local Proxy = require('session.proxy');
-local Item = require('session.item');
-local random = math.random;
+  Created by Masatoshi Teruya on 14/12/08.
+
+--]] -- module
+local Cookie = require('cookie')
+local siphash48 = require('siphash').encode48
+local gettimeofday = require('process').gettimeofday
+local typeof = require('util.typeof')
+local Proxy = require('session.proxy')
+local Item = require('session.item')
+local random = math.random
 -- constants
-local DEFAULT_TTL = 0;
+local DEFAULT_TTL = 0
 local DEFAULT_COOKIE = {
     name = 'sid',
     path = '/',
     httpOnly = true,
     secure = false
-};
+}
 
 -- init random
-math.randomseed( gettimeofday() );
+math.randomseed(gettimeofday())
 
 -- private
 local function DEFAULT_IDGEN()
-    return siphash48( gettimeofday(), tostring( random() ):sub( 1, 16 ) );
+    return siphash48(gettimeofday(), tostring(random()):sub(1, 16))
 end
 
-
 -- class
-local Session = require('halo').class.Session;
+local Session = require('halo').class.Session
 
+function Session:init(cfg)
+    local own = protected(self)
+    local err, t, _
 
-function Session:init( cfg )
-    local own = protected( self );
-    local err, t, _;
-    
-    if not typeof.table( cfg ) then
-        return nil, 'cfg must be table';
-    -- check cfg.store
-    elseif not typeof.table( cfg.store ) or 
-           not typeof.Function( cfg.store.get ) or
-           not typeof.Function( cfg.store.set ) or
-           not typeof.Function( cfg.store.delete ) then
-       return nil, 'cfg.store must implements get, set and delete methods';
+    if not typeof.table(cfg) then
+        return nil, 'cfg must be table'
+        -- check cfg.store
+    elseif not typeof.table(cfg.store) or not typeof.Function(cfg.store.get) or
+        not typeof.Function(cfg.store.set) or
+        not typeof.Function(cfg.store.delete) then
+        return nil, 'cfg.store must implements get, set and delete methods'
     end
-    own.store = cfg.store;
-    
+    own.store = cfg.store
+
     -- check cfg.ttl
-    own.ttl = cfg.ttl or DEFAULT_TTL;
-    if not typeof.uint( own.ttl ) then
-        return nil, 'cfg.ttl must be uint';
-    end
-    
+    own.ttl = cfg.ttl or DEFAULT_TTL
+    if not typeof.uint(own.ttl) then return nil, 'cfg.ttl must be uint' end
+
     -- check id generator
-    own.idgen = cfg.idgen or DEFAULT_IDGEN;
-    if not typeof.Function( own.idgen ) then
-        return nil, 'cfg.idgen must be function';
+    own.idgen = cfg.idgen or DEFAULT_IDGEN
+    if not typeof.Function(own.idgen) then
+        return nil, 'cfg.idgen must be function'
     end
     -- check generated value
-    _ = own.idgen();
-    if not typeof.string( _ ) or #_ < 1 then
-        return nil, 'cfg.idgen() must return non-empty string value';
+    _ = own.idgen()
+    if not typeof.string(_) or #_ < 1 then
+        return nil, 'cfg.idgen() must return non-empty string value'
     end
-    
+
     -- check cfg.cookie
     if cfg.cookie == nil then
-        cfg.cookie = {};
-    elseif not typeof.table( cfg.cookie ) then
-        return nil, 'cfg.cookie must be table';
+        cfg.cookie = {}
+    elseif not typeof.table(cfg.cookie) then
+        return nil, 'cfg.cookie must be table'
     end
     -- copy field values
-    own.cookie = {};
-    for k, v in pairs( DEFAULT_COOKIE ) do
+    own.cookie = {}
+    for k, v in pairs(DEFAULT_COOKIE) do
         if cfg.cookie[k] then
-            t = type( v );
-            if t ~= type( cfg.cookie[k] ) then
-                return nil, ('cfg.cookie.%s must be %s'):format( k, t );
+            t = type(v)
+            if t ~= type(cfg.cookie[k]) then
+                return nil, ('cfg.cookie.%s must be %s'):format(k, t)
             elseif t == 'string' and #cfg.cookie[k] < 1 then
-                return nil, ('cfg.cookie.%s must be non-empty string'):format( k );
+                return nil, ('cfg.cookie.%s must be non-empty string'):format(k)
             end
-            own.cookie[k] = cfg.cookie[k];
+            own.cookie[k] = cfg.cookie[k]
         else
-            own.cookie[k] = v;
+            own.cookie[k] = v
         end
     end
     -- set ttl to expires field
-    own.cookie.expires = cfg.ttl;
+    own.cookie.expires = cfg.ttl
     -- test create
-    _, err = Cookie.bake( own.cookie.name, '', own.cookie );
-    if err then
-        return nil, 'cfg.cookie.' .. err;
-    end
-    
-    return self;
+    _, err = Cookie.bake(own.cookie.name, '', own.cookie)
+    if err then return nil, 'cfg.cookie.' .. err end
+
+    return self
 end
 
+function Session:fetch(sid)
+    if not typeof.string(sid) then return nil, 'sid must be string' end
 
-function Session:fetch( sid )
-    if not typeof.string( sid ) then
-        return nil, 'sid must be string';
-    end
-    
-    return Item.new( protected( self ), sid );
+    return Item.new(protected(self), sid)
 end
 
+function Session:create() return Item.new(protected(self)) end
 
-function Session:create()
-    return Item.new( protected( self ) );
-end
-
-
-return Session.exports;
+return Session.exports
